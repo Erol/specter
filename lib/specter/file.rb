804d348b75ec71363b21cf69f5fe1873643476e0
Erol Fornoles <erol.fornoles@gmail.com>
2014-02-08 08:53:16 +0800

    Run the spec file and report errors and failures.

--- lib/specter/file.rb
+++ lib/specter/file.rb
@@ -7,5 +7,38 @@ class Specter
     def initialize(filename)
       @filename = filename
     end
+
+    def failed
+      @failed ||= []
+    end
+
+    def failed?
+      failed.any?
+    end
+
+    def run
+      fork do
+        begin
+          Specter.current.store :file, self
+
+          load filename
+
+        rescue Exception => exception
+          fail exception
+
+        ensure
+          Specter.current.delete :file
+
+          exit 1 if failed?
+        end
+      end
+    end
+
+    def fail(exception)
+      values = {file: Specter.current[:file]}
+      failed << values
+
+      puts exception.inspect
+    end
   end
 end
