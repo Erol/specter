fb4d9e292e1be391a61bec2a1fdb1a062529c892
Erol Fornoles <erol.fornoles@gmail.com>
2014-02-09 13:51:11 +0800

    Format and colorize progress.

--- lib/specter/reporter.rb
+++ lib/specter/reporter.rb
@@ -1,6 +1,24 @@
 class Specter
   module Reporter
+    COLORS = [
+      PASS = '32',
+      FAIL = '31'
+    ]
+
     def self.progress(values)
+      filename = values[:file].filename
+      description = values[:spec] && values[:spec].description
+
+      if exception = values[:exception]
+        puts "#{colorize(FAIL, 'FAILED')} #{description || filename}"
+        puts "  #{exception.class}: #{exception.message}"
+      else
+        puts "#{colorize(PASS, 'PASSED')} #{description}"
+      end
+    end
+
+    def self.colorize(color, text)
+      "\e[#{color}m#{text}\e[0m"
     end
   end
 end
