146cc92c89ac743765e798c178dc391e3228e952
Erol Fornoles <erol.fornoles@gmail.com>
2014-04-06 20:52:03 +0800

    Run in second level scope.

--- spec/scopes.rb
+++ spec/scopes.rb
@@ -5,3 +5,14 @@ spec 'run in file level scope' do
   assert scopes.size, :==, 1
   assert scopes[0].description, :==, file.name
 end
+
+scope 'first level scope' do
+  spec 'run in first level scope' do
+    file = Specter.now.file
+    scopes = Specter.now.scopes
+
+    assert scopes.size, :==, 2
+    assert scopes[0].description, :==, file.name
+    assert scopes[1].description, :==, 'first level scope'
+  end
+end
