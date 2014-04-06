f183624288b98db93433daa1421f8e16a689da1f
Erol Fornoles <erol.fornoles@gmail.com>
2014-04-06 21:25:15 +0800

    Call prepare blocks from file level scope to first level scope.

--- spec/prepare.rb
+++ spec/prepare.rb
@@ -15,3 +15,15 @@ spec 'call prepare blocks in file level scope only' do
   assert $first, :==, false
   assert $second, :==, false
 end
+
+scope 'first level scope' do
+  prepare do
+    $first = true
+  end
+
+  spec 'call prepare blocks from file level scope to first level scope' do
+    assert $file, :==, true
+    assert $first, :==, true
+    assert $second, :==, false
+  end
+end
