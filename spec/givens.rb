7d5dc1a502c2e3d783f999f1911f06ca975b59b9
Erol Fornoles <erol.fornoles@gmail.com>
2014-04-19 10:38:28 +0800

    Givens from first level scope are defined.

--- spec/givens.rb
+++ spec/givens.rb
@@ -5,3 +5,17 @@ end
 spec 'givens from file level scope are defined' do |given|
   assert given.file
 end
+
+scope 'first level scope' do
+  prepare do |given|
+    given.first = true
+  end
+
+  spec 'givens from file level scope are defined' do |given|
+    assert given.file
+  end
+
+  spec 'givens from first level scope are defined' do |given|
+    assert given.first
+  end
+end
